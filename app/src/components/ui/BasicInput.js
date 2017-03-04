1353917ab3b1f77f6fe9cb22b56ca92958bea2f0
edgar0011 <martin.weiser@gmail.com>
2017-03-04 23:28:08 +0100

    refactor(BasicInput): tiny updates

--- app/src/components/ui/BasicInput.js
+++ app/src/components/ui/BasicInput.js
@@ -1,58 +1,51 @@
-/**
- * Created by edgar on 11/01/2017.
- */
-import React from 'react';
-import lodash from 'lodash';
+import React from 'react'
+import _ from 'lodash'
 
 export default class BasicInput extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-
-    this.state = {value:this.props.value || "", errors:{}};
+  constructor (props) {
+    super(props)
+    this.state = {value: this.props.value || '', errors: {}}
   }
 
-  componentWillMount() {
-    const bounce = this.props.changeBounce === undefined ? 300 : this.props.changeBounce;
-    this._onChangeHandler = lodash.debounce(this._onChangeHandler, bounce);
+  componentWillMount () {
+    const bounce = this.props.changeBounce === undefined ? 300 : this.props.changeBounce
+    this._propsOnChangeHandler = _.debounce(this._propsOnChangeHandler, bounce)
   }
 
-  componentWillReceiveProps(nextProps) {
-    let {value} = nextProps;
+  componentWillReceiveProps (nextProps) {
+    let {value} = nextProps
     if (value !== this.props.value) {
-      this._setValue(value);
+      this._setValue(value)
     }
   }
 
-  onChangeHandler(event) {
-    let value = event.target.value;
+  onChangeHandler = (event) => {
+    let {value} = event.target
     if (this.props.formatters) {
-      this.props.formatters.forEach((formatter)=>{
-        value = formatter(value, this.state.value);
-      });
+      this.props.formatters.forEach((formatter) => {
+        value = formatter(value, this.state.value)
+      })
     }
-    this._setValue(value);
-    this._onChangeHandler();
+    this._setValue(value)
+    this._propsOnChangeHandler(value)
   }
 
-  _onChangeHandler(){
-    this.props.onChangeHandler(this.state);
+  _propsOnChangeHandler (value) {
+    this.props.onChangeHandler(value)
   }
 
-  reset() {
-    this._setValue("");
+  reset () {
+    this._setValue('')
   }
 
-  _setValue(value) {
-    this.setState({value});
+  _setValue (value) {
+    this.setState({value})
   }
 
-  render(){
-
-    const {value} = this.state;
-    const {props} = this;
+  render () {
+    const {value} = this.state
+    const {props} = this
 
     return (
       <input type={props.type || 'text'} class={props.className} onChange={this.onChangeHandler} value={value} pattern={props.pattern} />
@@ -65,4 +58,4 @@ BasicInput.propTypes = {
   onChangeHandler: React.PropTypes.func,
   type: React.PropTypes.string,
   value: React.PropTypes.any
-};
+}

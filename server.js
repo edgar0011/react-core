d665d43dc4b310ede290b5c58e6f59e6bf2ea2b6
edgar0011 <martin.weiser@gmail.com>
2018-12-19 22:26:50 +0100

    cors proxy buster

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 
 import express from 'express';
+import request from 'request';
 
 const app = express();
 
+app.use('/api', function(req, res) {
+  req.pipe(request(req.query.server + req.query.url)).pipe(res);
+});
+
 /* global __dirname */
 app.use(express.static(`${__dirname}/dist`));
+
 
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`server running at: ${server.address().port}`);

7525ef743a8310e007941d13dd12b8387269148e
edgar0011 <martin.weiser@gmail.com>
2019-04-11 12:22:01 +0200

    fix

--- serverApp/server.js
+++ serverApp/server.js
@@ -10,18 +10,12 @@ app.use('/api', function(req, res) {
 });
 
 /* global __dirname */
-app.use(express.static(`${__dirname}/dist`));
-
-app.use('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
-});
-
+app.use(express.static(path.join(__dirname, '../dist')));
 
 
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`server running at: ${server.address().port}`);
 });
-
 
 
 process.on('uncaughtException', function(err) {
